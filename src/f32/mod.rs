b2b569415d378a0bd25a16791f1f3c703f60c5df
Chen-Pang He <chen.pang.he@jdh8.org>
2024-08-05 16:17:23 +0800

    Slightly optimize range checking for f32::exp

--- src/f32/mod.rs
+++ src/f32/mod.rs
@@ -3,17 +3,18 @@ mod kernel;
 /// The exponential function
 #[must_use]
 pub fn exp(x: f32) -> f32 {
+    use core::f32::consts::LN_2;
     use core::f64::consts;
-    let x = f64::from(x);
 
-    if x <= -150.0 * consts::LN_2 {
+    if x < -150.0 * LN_2 {
         return 0.0;
     }
 
-    if x >= 128.0 * consts::LN_2 {
+    if x > 128.0 * LN_2 {
         return f32::INFINITY;
     }
 
+    let x = f64::from(x);
     let n = (x * consts::LOG2_E).round_ties_even();
     let y = kernel::exp(n.mul_add(-consts::LN_2, x)).mul_add(x, 1.0);
 
