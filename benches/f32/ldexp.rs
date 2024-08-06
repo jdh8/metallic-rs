50ff05c7acdbf0e6c53fabfd53be6f937636457a
Chen-Pang He <chen.pang.he@jdh8.org>
2024-08-07 03:46:05 +0800

    Remove a turbofish with type inference

--- benches/f32/ldexp.rs
+++ benches/f32/ldexp.rs
@@ -4,9 +4,8 @@ use rand::Rng as _;
 fn bench(criterion: &mut Criterion, id: &str, f: fn(f32, i32) -> f32) {
     criterion.bench_function(id, |bencher| {
         let rng = &mut rand::thread_rng();
-
         bencher.iter_batched(
-            || (rng.gen::<f32>(), rng.gen_range(-300..300)),
+            || (rng.gen(), rng.gen_range(-300..300)),
             |(x, n)| f(x, n),
             BatchSize::SmallInput,
         );
