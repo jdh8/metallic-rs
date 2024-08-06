ae7ed70757e7c9e9f7be8e579b0fd607d532ff45
Chen-Pang He <chen.pang.he@jdh8.org>
2024-08-07 04:43:47 +0800

    Harden tests by decreasing the steps

--- tests/f32/ldexp.rs
+++ tests/f32/ldexp.rs
@@ -1,10 +1,10 @@
-use metallic::f32 as lib;
+use metallic::f32 as metal;
 
 fn up(x: f32) {
     let mut y = x;
 
     for n in 0..300 {
-        assert!(lib::ldexp(x, n).eq(&y));
+        assert!(metal::ldexp(x, n).eq(&y));
         y *= 2.0;
     }
 }
@@ -15,7 +15,7 @@ fn down(x: f32) {
     for n in 0..300 {
         #[allow(clippy::cast_possible_truncation)]
         let y = (f64::from(x) * coefficient) as f32;
-        assert!(lib::ldexp(x, -n).eq(&y));
+        assert!(metal::ldexp(x, -n).eq(&y));
         coefficient *= 0.5;
     }
 }
@@ -31,13 +31,13 @@ fn run(x: f32) {
 fn test_ldexp() {
     run(f32::INFINITY);
 
-    (0..0x7F00_0000).step_by(71463).for_each(|i| {
+    (0..0x7F00_0000).step_by(7146).for_each(|i| {
         run(f32::from_bits(i));
     });
 
-    (0x7FC0_0000..0x8000_0000).step_by(98765).for_each(|i| {
+    (0x7FC0_0000..0x8000_0000).step_by(9876).for_each(|i| {
         let x = f32::from_bits(i);
-        assert!(lib::ldexp(x, 7).is_nan());
-        assert!(lib::ldexp(x, -7).is_nan());
+        assert!(metal::ldexp(x, 7).is_nan());
+        assert!(metal::ldexp(x, -7).is_nan());
     });
 }
--- tests/f32/main.rs
+++ tests/f32/main.rs
@@ -1,21 +1,21 @@
 mod ldexp;
 
-use metallic::f32 as lib;
+use metallic::f32 as metal;
 
 fn is_faithful_rounding(result: f32, expected: f64) -> bool {
     if expected.is_nan() {
         return result.is_nan();
     }
 
-    let next_up = f64::from(lib::next_up(result));
-    let next_down = f64::from(lib::next_down(result));
+    let next_up = f64::from(metal::next_up(result));
+    let next_down = f64::from(metal::next_down(result));
     next_down <= expected && expected <= next_up
 }
 
 #[test]
 fn test_exp() {
-    (0..u32::MAX).step_by(777).for_each(|i| {
+    (0..u32::MAX).step_by(77).for_each(|i| {
         let x = f32::from_bits(i);
-        assert!(is_faithful_rounding(lib::exp(x), f64::from(x).exp()));
+        assert!(is_faithful_rounding(metal::exp(x), f64::from(x).exp()));
     });
 }
