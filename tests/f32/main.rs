d87d2e2904c4f1bb6b9806e8f8f9be7b10ebf8c8
Chen-Pang He <chen.pang.he@jdh8.org>
2024-08-07 14:42:23 +0800

    Test f32::frexp

--- tests/f32/main.rs
+++ tests/f32/main.rs
@@ -1,4 +1,5 @@
 mod ldexp;
+use core::num::FpCategory;
 use metallic::f32 as metal;
 
 fn is_faithful_rounding(result: f32, expected: f64) -> bool {
@@ -8,7 +9,7 @@ fn is_faithful_rounding(result: f32, expected: f64) -> bool {
 
     let next_up = f64::from(metal::next_up(result));
     let next_down = f64::from(metal::next_down(result));
-    next_down <= expected && expected <= next_up
+    (next_down..=next_up).contains(&expected)
 }
 
 #[test]
@@ -16,5 +17,26 @@ fn test_exp() {
     (0..u32::MAX).step_by(77).for_each(|i| {
         let x = f32::from_bits(i);
         assert!(is_faithful_rounding(metal::exp(x), f64::from(x).exp()));
+    });
+}
+
+#[test]
+fn test_frexp() {
+    (0..u32::MAX).step_by(69).for_each(|i| {
+        let x = f32::from_bits(i);
+        let (significand, exponent) = metal::frexp(x);
+        
+        match x.classify() {
+            FpCategory::Nan => assert!(significand.is_nan()),
+            FpCategory::Infinite => assert_eq!(significand.to_bits(), x.to_bits()),
+            FpCategory::Zero => {
+                assert_eq!(significand.to_bits(), x.to_bits());
+                assert_eq!(exponent, 0);
+            }
+            _ => {
+                assert!((0.5..1.0).contains(&significand.abs()));
+                assert_eq!(metal::ldexp(significand, exponent).to_bits(), x.to_bits());
+            }
+        }
     });
 }
