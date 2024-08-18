42a0fdfacf1f801750ac689a17a618ba2500ada2
Chen-Pang He <chen.pang.he@jdh8.org>
2024-08-19 04:36:39 +0800

    Harden testing f32::ldexp by breaking down to units

--- tests/f32/ldexp.rs
+++ tests/f32/ldexp.rs
@@ -1,18 +1,18 @@
 use metallic::f32 as metal;
 
-fn up(x: f32) {
+fn check_up(x: f32) {
     let mut y = x;
 
-    for n in 0..300 {
+    for n in 0..=300 {
         assert_eq!(metal::ldexp(x, n).to_bits(), y.to_bits());
         y *= 2.0;
     }
 }
 
-fn down(x: f32) {
-    let mut coefficient = 1.0;
+fn check_down(x: f32) {
+    let mut coefficient = 0.5;
 
-    for n in 0..300 {
+    for n in 1..=300 {
         #[allow(clippy::cast_possible_truncation)]
         let y = (f64::from(x) * coefficient) as f32;
         assert_eq!(metal::ldexp(x, -n).to_bits(), y.to_bits());
@@ -20,24 +20,47 @@ fn down(x: f32) {
     }
 }
 
-fn run(x: f32) {
-    up(x);
-    up(-x);
-    down(x);
-    down(-x);
+#[test]
+fn test_finite_positive_up() {
+    (0..f32::INFINITY.to_bits()).step_by(17).for_each(|i| {
+        check_up(f32::from_bits(i));
+    });
 }
 
 #[test]
-fn test() {
-    run(f32::INFINITY);
-
-    (0..0x7F00_0000).step_by(69).for_each(|i| {
-        run(f32::from_bits(i));
+fn test_finite_positive_down() {
+    (0..f32::INFINITY.to_bits()).step_by(17).for_each(|i| {
+        check_down(f32::from_bits(i));
     });
+}
 
-    (0x7FC0_0000..0x8000_0000).step_by(73).for_each(|i| {
+#[test]
+fn test_finite_negative_up() {
+    (0..f32::INFINITY.to_bits()).step_by(17).for_each(|i| {
+        check_up(-f32::from_bits(i));
+    });
+}
+
+#[test]
+fn test_finite_negative_down() {
+    (0..f32::INFINITY.to_bits()).step_by(17).for_each(|i| {
+        check_down(-f32::from_bits(i));
+    });
+}
+
+#[test]
+fn test_infinite() {
+    for n in -300..=300 {
+        assert!(metal::ldexp(f32::INFINITY, n).eq(&f32::INFINITY));
+        assert!(metal::ldexp(f32::NEG_INFINITY, n).eq(&f32::NEG_INFINITY));
+    }
+
+    (f32::INFINITY.to_bits() + 1..0x8000_0000).for_each(|i| {
         let x = f32::from_bits(i);
-        assert!(metal::ldexp(x, 7).is_nan());
-        assert!(metal::ldexp(x, -7).is_nan());
+
+        for n in -300..=300 {
+            assert!(metal::ldexp(x, n).is_nan());
+            assert!(metal::ldexp(-x, n).is_nan());
+        }
     });
 }
