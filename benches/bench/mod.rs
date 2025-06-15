1e8a0945627632c76f50e69b35312e3c79082dfc
Chen-Pang He <chen.pang.he@jdh8.org>
2025-06-15 18:49:27 +0800

    Robust handling trailing commas

--- benches/bench/mod.rs
+++ benches/bench/mod.rs
@@ -1,22 +1,22 @@
 #[macro_export]
 macro_rules! bench {
-    ($name:ident, $callback:expr, $($args:expr),*) => {
+    ($name:ident, $callback:expr $(, $args:expr)* $(,)?) => {
         fn $name(criterion: &mut criterion::Criterion) {
             criterion.bench_function(stringify!($callback), |bencher| {
                 bencher.iter(|| $callback($($args),*));
             });
         }
     };
-    ($name:ident, $callback:expr, _) => {
+    ($name:ident, $callback:expr, _ $(,)?) => {
         bench!($name, $callback, rand::random());
     };
-    ($name:ident, $callback:expr, _, _) => {
+    ($name:ident, $callback:expr, _, _ $(,)?) => {
         bench!($name, $callback, rand::random(), rand::random());
     };
-    ($name:ident, $callback:expr, in $range:expr) => {
+    ($name:ident, $callback:expr, in $range:expr $(,)?) => {
         bench!($name, $callback, rand::random_range($range));
     };
-    ($name:ident, $callback:expr, _, in $range:expr) => {
+    ($name:ident, $callback:expr, _, in $range:expr $(,)?) => {
         bench!($name, $callback, rand::random(), rand::random_range($range));
     };
 }
